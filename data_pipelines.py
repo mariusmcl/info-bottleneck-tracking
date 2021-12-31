aab0d76f745792e9d68baf6af53c2f9e4c5e1a75
Marius Landsverk <marius_cl@hotmail.com>
2021-12-31 18:00:38 +0100

    dataset reduction

--- data_pipelines.py
+++ data_pipelines.py
@@ -22,22 +22,44 @@ def get_data_and_indices(dataset_name, reduction_factor=None):
         splits["train"] = splits["train"][::REDUCTION_FACTOR]   # Went out of memory (33GB) when comupting full
         splits["valid"] = splits["valid"][::REDUCTION_FACTOR]   
         
-        print(splits["valid"].shape)
-        print(splits["train"].shape)
         idx_to_run = torch.cat((splits["train"], splits["valid"]), 0)
-        print(idx_to_run.shape)
-        
+        ss = set(list(idx_to_run.numpy()))
+        lookup_dict = {}
+        for j in range(idx_to_run.shape[0]):
+            value = int(idx_to_run[j])
+            lookup_dict[value] = j   # Map the previous index to the current index
+        new_edge_index = []
+
+        print("Removing edges")
+        for j in range(arxiv_dataset.data.edge_index.shape[1]):
+            a, b = int(arxiv_dataset.data.edge_index[0, j].numpy()), int(arxiv_dataset.data.edge_index[1, j].numpy())
+            if (a in ss) and (b in ss):
+                assert (a in lookup_dict.keys()) and (b in lookup_dict.keys())
+                a, b = lookup_dict[a], lookup_dict[b]
+                new_edge_index.append([a, b])
+        print("Finished removing edges")
+        qq = np.array(new_edge_index)
+        qq = torch.from_numpy(qq).type_as(arxiv_dataset.data.edge_index).reshape((2, -1)).contiguous()
+
         print("dataset shape before reduction:")
         print(arxiv_dataset.data.x.shape)
         print(arxiv_dataset.data.y.shape)
+        print(arxiv_dataset.data.edge_index.shape)
         arxiv_dataset.data.x = arxiv_dataset.data.x[idx_to_run]
         arxiv_dataset.data.y = arxiv_dataset.data.y[idx_to_run]
+        arxiv_dataset.data.edge_index = qq
         print("dataset shape after reduction:")
         
         print(arxiv_dataset.data.x.shape)
         print(arxiv_dataset.data.y.shape)
-        
-        
+        print(arxiv_dataset.data.edge_index.shape)
+
+        train_indices = torch.from_numpy(np.array([i for i in range(splits["train"].shape[0])])).type_as(splits["train"])
+        valid_indices = torch.from_numpy(np.array([i + train_indices.shape[0] for i in range(splits["valid"].shape[0])])).type_as(splits["valid"])
+
+        splits["train"] = train_indices
+        splits["valid"] = valid_indices
+
         arxiv_dataset.data.y = arxiv_dataset.data.y.squeeze()
         model_params = {"num_classes":40, "num_features": 128}
         return arxiv_dataset.data, splits, model_params
